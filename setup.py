6d095223d9160ce9827695f76cce8b1d065654a9
Theeojeong <wogusto13@gmail.com>
2026-01-16 22:47:08 +0900

    v0.1.5: Fix version + workflow checkout

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="llm-chunker",
-    version="0.1.4",
+    version="0.1.5",
     description="A semantic and legal text chunker based on LLM analysis",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
